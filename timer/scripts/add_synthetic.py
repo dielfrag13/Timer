7812bd34a1920aa228744a83524554f858e4c4db
Kyle Buchmiller <kyle.buchmiller@gmail.com>
2025-03-13 18:12:45 -0400

    synthetic data stuff.... bug on rendering elapsed time

--- timer/scripts/add_synthetic.py
+++ timer/scripts/add_synthetic.py
@@ -1,29 +1,61 @@
 import datetime
-import timer.models
+from timer.models import *
+import random
+
 
 
 def run():
     print("adding synthetic data!")
-    s1, _ = timer.models.SurgeonEntry.objects.get_or_create(first_name="Tom", last_name="Foolery", email="tom.foolery@lol.gov")
-    s2, _ = timer.models.SurgeonEntry.objects.get_or_create(first_name="Darrin", last_name="Trask", email="dirtydog69@aol.com")
-    s3, _ = timer.models.SurgeonEntry.objects.get_or_create(first_name="Katty", last_name="Wampus", email="inconsistent@gmail.xyz")
+    s1, _ = Surgeon.objects.get_or_create(first_name="Tom", last_name="Foolery", email="tom.foolery@lol.gov")
+    s2, _ = Surgeon.objects.get_or_create(first_name="Darrin", last_name="Trask", email="dirtydog69@aol.com")
+    s3, _ = Surgeon.objects.get_or_create(first_name="Katty", last_name="Wampus", email="inconsistent@gmail.xyz")
 
-    o1, _ = timer.models.OperationEntry.objects.get_or_create(operation_type="Sixth Finger Augmentation")
-    o2, _ = timer.models.OperationEntry.objects.get_or_create(operation_type="Synthetic Femur Upgrade")
+    o1, _ = OperationType.objects.get_or_create(operation_type="Sixth Finger Augmentation")
+    o2, _ = OperationType.objects.get_or_create(operation_type="Synthetic Femur Upgrade")
+    o3, _ = OperationType.objects.get_or_create(operation_type="Total Knee")
 
+    # create steps
+    step_list = ["in-room", "block done", "tubed", "prepped/draped", "time-out", "incision", "parts in", "closure end", "out of room"]
+    for s in step_list:
+        Step.objects.get_or_create(title=s)
     
-    timer.models.TimerEntry.objects.all().delete()
-    timer.models.TimerEntry.objects.get_or_create(
-        title="Paul's First Sixth Finger", 
-        operation=o1,
-        date=datetime.date(2024, 12, 12),
-        start_time=datetime.time(7, 30, 20),
-        end_time=datetime.time(9, 12, 26),
-        detail="Today we gave Paul a sixth finger on his right hand.",
-        surgeon=s1,
+    OperationInstance.objects.all().delete()
+    print("created steps!")
+    import code
+    code.interact(local=locals())
+    oi1 = OperationInstance(
+        operation_type=o3,
+        date=datetime.date(2025, 3, 13),
+        detail="Today we gave Paul a fresh robo knee",
+        surgeon=s2,
+        ocs1 = True,
+        complete = True,
     )
+    oi1.save()
+    # add timed steps where each step takes a random amount of time between 20 and 60 minutes 
+    start_time = datetime.datetime(2025, 3, 13, 7, 30, 20)
+    end_time = start_time + datetime.timedelta(minutes=random.randint(20, 60))
 
-    timer.models.TimerEntry.objects.get_or_create(
+    print("created operation instance")
+    import code
+    code.interact(local=locals())
+    for i, s in enumerate(step_list):
+        si = StepInstance(
+            step=Step.objects.get(title=s),
+            operation_instance=oi1,
+            order=i+1,
+            start_time = start_time.time(),
+            end_time = end_time.time(),
+        )
+        si.save()
+        start_time = end_time
+        end_time += datetime.timedelta(minutes=random.randint(20,60))
+
+    oi1.save()        
+    """
+    start_time=datetime.time(7, 30, 20),
+    end_time=datetime.time(9, 12, 26),
+    OperationInstance.objects.get_or_create(
         title="Paul's Second Sixth Finger", 
         operation=o1,
         date=datetime.date(2025, 1, 17),
@@ -33,7 +65,7 @@ def run():
         surgeon=s2,
     )
 
-    timer.models.TimerEntry.objects.get_or_create(
+    OperationInstance.objects.get_or_create(
         title="Paul's new SmartFemur", 
         operation=o2,
         date=datetime.date(2025, 1, 5),
@@ -43,7 +75,7 @@ def run():
         surgeon=s1,
     )
 
-    timer.models.TimerEntry.objects.get_or_create(
+    OperationInstance.objects.get_or_create(
         title="Lisa's new SmartFemur", 
         operation=o2,
         date=datetime.date(2025, 1, 9),
@@ -52,3 +84,5 @@ def run():
         detail="Paul loved his SmartFemur so much that Lisa asked for one too!",
         surgeon=s2,
     )
+
+    """
