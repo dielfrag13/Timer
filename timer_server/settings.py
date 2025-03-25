33d7874209d8c33752fa9c34e291ed210d9babf8
Kyle Buchmiller <kyle.buchmiller@gmail.com>
2025-03-25 12:30:26 -0400

    completed/verified install script works, updated requirements for windows machines, modified settings.py to be for first release, add zip server command script for pulling necessary files

--- timer_server/settings.py
+++ timer_server/settings.py
@@ -23,9 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2o@r%@(&xg-srv-7nx&p0u7%b2sz4j(wz&i%$^bks@f&@n#%qh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    # "your-domain-here.com",
+    # "www.your-domain-here.com",
+]
 
 
 # Application definition
@@ -43,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,7 +131,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'static_files'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# additional things identified from python manage.py check --deploy
+
+SECURE_SSL_REDIRECT = False 
